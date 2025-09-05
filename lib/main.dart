061b4872085ba84cfc1227f4d429eb3a228ffcc3
Jainish-Mehta <jainishmehta2005@outlook.com>
2025-09-05 23:37:09 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,32 @@
+import 'package:autoricksaw/autoricksaw_home_page.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MyApp()); //const is used bcoz we don't need to recreate widget or constructor every time 
+void main() {
+  runApp(
+    const MyApp(),
+  ); //const is used bcoz we don't need to recreate widget or constructor every time
 }
 
-/* Statelesswidget data inside them are immutable.
-   Statefullwidget data inside them are mutable.
-   InheritedWidget
-   */
-  
-class MyApp extends StatelessWidget{
-  const MyApp(
-    {
-      super.key
-    }
-  );
+// StatelessWidget: Immutable, ideal for static UI
+// StatefulWidget: Mutable, use when UI changes dynamically
+// InheritedWidget: Used for propagating data down the widget tree efficiently
+
+/*
+ Design:
+ 1.Material Design provided by Google for Android
+ 2.Cupertino Design provided by Apple for IOS
+  */
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // MaterialApp is a widget wraps your entire app and sets up the Material Design environment.
+  // Think of it as the entry point that wires up theming, navigation, and localization.
+
+  //Scaffold is a widget provides the basic visual structure for each screen.
+  //It’s what gives you the app bar, drawer, floating action button, and body content.
+
   @override
   Widget build(BuildContext context) {
-    return const Text(
-    'Hello world',textDirection: TextDirection.ltr,
-    );
+    return MaterialApp(home: AutoricksawHomePage());
   }
 }
